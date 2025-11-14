8ed505e6bc705fd8fa978e31ae0da76295a5b533
Robin Müller <robin.mueller1@gmx.ch>
2025-11-14 15:22:13 +0100

    adding overview-page

---
+++ swu-collection/src/App.jsx
@@ -0,0 +1,12 @@
+import SetLoader from './components/SetLoader'
+import './App.css'
+
+function App() {
+  return (
+  <body>
+    <section className="App"><SetLoader set="SOR" /></section>
+  </body>
+  )
+}
+
+export default App
